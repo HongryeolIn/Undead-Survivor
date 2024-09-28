47b541981e8fcf2cbe20ba7341c690569034a860
Hongryeol In <phbzti924@naver.com>
2024-09-29 01:17:42 +0900

    플레이어 애니메이션 구현
    가만히 있을때, 움직일때, 죽었을때

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,17 @@ public class Player : MonoBehaviour
 {
     public Vector2 InputVec;
     public float Speed;
+
     Rigidbody2D Rigid;
+    SpriteRenderer Spriter;
+    Animator Anim;
 
     void Awake()
     {
         Rigid = GetComponent<Rigidbody2D>();
-        Speed = 1;
+        Spriter = GetComponent<SpriteRenderer>();
+        Anim = GetComponent<Animator>();
+        Speed = 3;
     }
 
     void Start()
@@ -32,5 +37,18 @@ public class Player : MonoBehaviour
     {
         Vector2 NextVec =  InputVec * Speed * Time.fixedDeltaTime;
         Rigid.MovePosition(Rigid.position + NextVec);
+    }
+
+    private void LateUpdate()
+    {
+        Anim.SetFloat("Speed", InputVec.magnitude);
+
+        //키를 눌렀을때
+        if(InputVec.x != 0)
+        {
+            //방향에 따라 플레이어의 스프라이트렌더러 Flip X를 컨트롤
+            Spriter.flipX = InputVec.x < 0;
+
+        }
     }
 }

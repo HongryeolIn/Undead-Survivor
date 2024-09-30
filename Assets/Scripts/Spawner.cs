0e4e8f9af75895503357d0b1bec664e85ca0fe25
Hongryeol In <phbzti924@naver.com>
2024-09-30 23:50:07 +0900

    풀링으로 적 랜덤스폰 구현

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Transform[] SpawnPoint;
+
+    float timer;
+    float spawnRate = 0.2f;
+
+    private void Awake()
+    {
+        SpawnPoint = GetComponentsInChildren<Transform>();
+    }
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        timer += Time.deltaTime;
+        if(timer >= spawnRate)
         {
-            GameManager.instance.Pool.Get(0);
+            Spawn();
+            timer = 0f;
         }
+    }
+    
+    void Spawn()
+    {
+        GameObject enemy = GameManager.instance.Pool.Get(Random.Range(0,2));
+        enemy.transform.position = SpawnPoint[Random.Range(1,SpawnPoint.Length)].position;
     }
 }
